a7c65e32002019035a156bc09eaefba5d1bad461
Drepzin <dreponexd@gmail.com>
2024-06-22 22:59:45 -0300

    Inicio do projeto!

---
+++ src/main/java/com/RpgApi/MCRpg/McRpgApplication.java
@@ -0,0 +1,13 @@
+package com.RpgApi.MCRpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McRpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RpgApi/MCRpg/McRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RpgApi.MCRpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McRpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
